6a681ba5a7d672590a71dff9956da719d56d1431
salk01 <102027438+salk01@users.noreply.github.com>
2022-03-21 15:52:27 +0100

    Update three.js

--- three/three.js
+++ three/three.js
@@ -2,7 +2,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth - 50, window.innerHeight - 50);
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry();
